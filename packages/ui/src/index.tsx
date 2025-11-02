74ccb0129016be196c2b63d0b78396646deaffad
soyres <respizarro3@gmail.com>
2025-11-02 14:15:12 -0700

    fix typescript error

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,15 +1,20 @@
 
-// Components
+// Named exports (components that use "export const")
+export { Hero } from './Hero';
+export { Section } from './Section';
+export { SplashIntro } from './SplashIntro';
+
+// Default exports (components that use "export default")
 export { default as NavBar } from './NavBar';
-export { default as Hero } from './Hero';
-export { default as Section } from './Section';
 export { default as ContactForm } from './ContactForm';
 export { default as SlideDrawer } from './SlideDrawer';
-export { default as SplashIntro } from './SplashIntro';
 export { default as PageShell } from './PageShell';
 export { default as OptimizedImage } from './OptimizedImage';
 export { default as OptimizedVideo } from './OptimizedVideo';
 export { default as Analytics } from './Analytics';
+export { default as HamburgerButton } from './HamburgerButton';
+export { default as NavBarLinks } from './NavBarLinks';
+export { default as HeroNavOverlay } from './HeroNavOverlay';
 
 // Icons
 export * from './Icons';
